f29660ad9cd3df0930863b6d83a201574140c349
Fabrizio <fabrizio.venturini@optrail.com>
2024-07-04 22:40:16 +0200

    added tests

--- start.py
+++ start.py
@@ -9,13 +9,17 @@ if len(sys.argv) <= 1:
     print('Please provide a package name!')
     sys.exit(1)
 
+onStart = ''
+if len(sys.argv) > 2:
+    onStart = sys.argv[2]
+
 sys.path.append(sys.argv[1])
 
 m = importlib.import_module('src')
 
 
 game = monkey.engine()
-game.start(m)
+game.start(m, on_start=onStart)
 game.run()
 game.shutdown()
 
