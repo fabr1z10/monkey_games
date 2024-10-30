082c1d7a51f5bf773ebfde8ff67d595b3251a52d
Fabrizio <fabrizio.venturini@optrail.com>
2024-10-30 22:00:34 +0100

    platformer

--- test_platformer/src/factory.py
+++ test_platformer/src/factory.py
@@ -1,6 +1,22 @@
 import monkey
 import math
 
+FLAG_PLAYER = 1
+FLAG_PLATFORM = 2
+
+# moving platform
+def make_moving_platform(p0, delta, time):
+  mp = monkey.Node()
+  mp.add_component(monkey.components.Collider(FLAG_PLATFORM, FLAG_PLAYER, 1, monkey.shapes.Rect(32, 8)))
+  mover = monkey.components.Mover()
+  mover.add(monkey.actions.Move(0, p0, 0))
+  mover.add(monkey.actions.MoveBy(0, delta, time))
+  mp.add_component(mover)
+  #mp.set_position(160, 120)
+  return mp
+
+
+
 def create_room(room):
   world_size = (512, 240)
   cam = monkey.CamOrtho(256, 240,
@@ -13,25 +29,38 @@ def create_room(room):
 
   # adding a platform
   platform1 = monkey.Node()
-  platform1.add_component(monkey.components.Collider(2, 0, 1, monkey.shapes.AABB(0, 400, 0, 32)))
+  platform1.add_component(monkey.components.Collider(FLAG_PLATFORM, 0, 1, monkey.shapes.AABB(0, 320, 0, 32)))
   root.add(platform1)
 
   platform2 = monkey.Node()
-  platform2.add_component(monkey.components.Collider(2, 0, 1, monkey.shapes.AABB(160, 240, 0, 64)))
+  platform2.add_component(monkey.components.Collider(FLAG_PLATFORM, 0, 1, monkey.shapes.AABB(160, 240, 0, 64)))
   root.add(platform2)
 
   platform3 = monkey.Node()
-  platform3.add_component(monkey.components.Collider(2, 0, 1, monkey.shapes.AABB(0, 16, 0, 64)))
+  platform3.add_component(monkey.components.Collider(FLAG_PLATFORM, 0, 1, monkey.shapes.AABB(0, 16, 0, 64)))
   root.add(platform3)
 
   platform4 = monkey.Node()
-  platform4.add_component(monkey.components.Collider(2, 0, 1, monkey.shapes.ConvexPoly([0,0,160,0,160,30])))
-  platform4.set_position(360,32)
+  platform4.add_component(monkey.components.Collider(FLAG_PLATFORM, 0, 1, monkey.shapes.ConvexPoly([0,0,160,0,160,30])))
+  platform4.set_position(320,32)
   root.add(platform4)
 
+  # moving platform
+  root.add(make_moving_platform((160, 64), (128, 0), 3.0))
+  root.add(make_moving_platform((140, 64), (0, 128), 3.0))
+  # mp = monkey.Node()
+  # mp.add_component(monkey.components.Collider(FLAG_PLATFORM, FLAG_PLAYER, 1, monkey.shapes.Rect(32, 8)))
+  # mover = monkey.components.Mover()
+  # mover.add(monkey.actions.Move(0, (160, 64), 0))
+  # mover.add(monkey.actions.MoveBy(0, (128, 0), 3.0))
+  # mp.add_component(mover)
+  # mp.set_position(160, 120)
+  # root.add(mp)
 
   player = monkey.Node()
-  player.set_position(100, 128)
-  player.add_component(monkey.components.Controller2D(size=(16,16), gravity=30, speed=100, acceleration=500))
+  player.set_position(32, 128)
+  player.add_component(monkey.components.Collider(FLAG_PLAYER, 0, 0, monkey.shapes.AABB(-8, 8, 0, 16)))
+  player.add_component(monkey.components.Controller2D(size=(16,16), speed=100, acceleration=500,
+                                                      jump_height=128, time_to_jump_apex=1))
   player.add_component(monkey.components.Follow(0))
   root.add(player)
