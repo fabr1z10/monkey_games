ca9bb22713f48a2878c8e8dc91ee4bcac6e8a1ee
fabr1z10 <fabventurini@gmail.com>
2025-03-24 22:40:43 +0100

    updated newgame

--- newgame.sh
+++ newgame.sh
@@ -8,20 +8,17 @@ mkdir $1
 mkdir $1/src
 mkdir $1/assets
 cat > $1/src/__init__.py << EOF
-from .settings import *
 from .factory import *
 EOF
 
 cat > $1/main.py << EOF
 #!/usr/bin/python3
 
-import monkey
-import src
+import monkey2
 
-game = monkey.engine()
-game.start(src)
+game = monkey2.game()
+game.start()
 game.run()
-game.shutdown()
 EOF
  
 #cat > $1/factory.py << EOF
@@ -29,22 +26,19 @@ EOF
 #    pass
 #EOF
 
-cat > $1/src/settings.py << EOF
-import monkey
+cat > $1/settings.py << EOF
+import monkey2
 
-device_size = (256, 240)
+device_size = monkey2.IVec2(256, 240)
 title = 'New game'
-shaders = [
-    monkey.SHADER_BATCH_QUAD_PALETTE,
-    monkey.SHADER_BATCH_LINES
-]
 EOF
 
 cat > $1/src/factory.py << EOF
-import monkey
+import monkey2
 
-def create_room(room):
-  pass
+def create_room():
+    room = monkey2.Room()
+    return room
 EOF
 
 
